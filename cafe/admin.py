b0e25070e2138b888aef3c02c7f8d3aba8e435d3
mohamad <mohamad1shabanian@gmail.com>
2024-06-10 15:18:36 +0330

    Added models and admin permissions

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Admin, Product, Order, OrdersProduct, UsersOrders, Storage
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Admin)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrdersProduct)
+admin.site.register(UsersOrders)
+admin.site.register(Storage)
+
--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return self.username
+
+
+class Admin(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    sugar = models.IntegerField()
+    coffee = models.IntegerField()
+    flour = models.IntegerField()
+    chocolate = models.IntegerField()
+    egg = models.IntegerField()
+    vertical = models.BinaryField(max_length=1)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    purchase_amount = models.IntegerField()
+    type = models.BinaryField(max_length=1)
+
+    def __str__(self):
+        return str(self.order_id)
+
+
+class OrdersProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Order {self.order.order_id} - Product {self.product.name}'
+
+
+class UsersOrders(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Order {self.order.order_id} - Product {self.user.username}'
+
+
+class Storage(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.name
